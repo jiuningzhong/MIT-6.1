906e89218471d7bdda96ad7633051a2caf409fdf
jiuningzhong <johnny0109@gmail.com>
2019-01-24 23:47:08 -0500

    ddd

--- Hello.py
+++ Hello.py
@@ -60,7 +60,7 @@ data = [{'first':'Guido', 'last':'Van Rossum', 'YOB':1956},
 {'first':'Grace', 'last':'Hopper', 'YOB':1906},
 {'first':'Alan', 'last':'Turing', 'YOB':1912}]
 
-sorted([2,4,3,5,1,6])
+sorted([2,4,3,5,1,7,98,93,6])
 
 sorted(data, key=lambda item: item['first'])
 sorted(data, key=lambda item: item['YOB'])
